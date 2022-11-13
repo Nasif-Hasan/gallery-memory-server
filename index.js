3d945731e6a4eb1ef9a33000b9304661058ca379
Nasif-Hasan <nasifhasan35@gmail.com>
2022-11-13 10:27:12 +0600

    patch

--- index.js
+++ index.js
@@ -51,6 +51,19 @@ async function run() {
             res.send(result)
         })
 
+        app.patch('/orders/:id', async(req, res) => {
+            const id = req.params.id
+            const status = req.body.status
+            const query = { _id: ObjectId(id)}
+            const updatedDoc = {
+                $set:{
+                    status: status
+                }
+            }
+            const result = await orderCollection.updatedOne(query, updatedDoc)
+            res.send(result)
+        })
+
         app.delete('/orders/:id', async(req, res) => {
             const id = req.params.id
             const query = {_id: ObjectId(id)}
